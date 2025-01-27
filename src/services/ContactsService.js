bd32c64d5d5190c4354026086a10546f0a32f0c9
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-01-27 12:07:08 -0300

    refactor: change http request handling to facilitate maintenance

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -1,13 +1,12 @@
-import delay from '../utils/delay';
+import HttpClient from './utils/HttpClient';
 
 class ContactsService {
-  async listContacts(orderBy = 'asc') {
-    const response = await fetch(
-      `http://localhost:3001/contacts?orderBy=${orderBy}`,
-    );
+  constructor() {
+    this.httpClient = new HttpClient('http://localhost:3001');
+  }
 
-    await delay();
-    return response.json();
+  async listContacts(orderBy = 'asc') {
+    return this.httpClient.get(`/contacts?orderBy=${orderBy}`);
   }
 }
 
