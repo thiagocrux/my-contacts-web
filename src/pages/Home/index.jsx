6dd7660f3a31edf392f596254f61a91b90c7026e
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-01-30 11:07:29 -0300

    feat: implement empty contacts list and invalid search logic and visual messages

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,24 @@
-import { useEffect, useState, useMemo } from 'react';
+import { useEffect, useState, useMemo, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
-import trash from '../../assets/images/icons/trash.svg';
+import emptyBox from '../../assets/images/icons/empty-box.svg';
+import magnifierQuestion from '../../assets/images/icons/magnifier-question.svg';
 import sad from '../../assets/images/icons/sad.svg';
+import trash from '../../assets/images/icons/trash.svg';
 import Button from '../../components/Button';
 import Loader from '../../components/Loader';
 
 import {
-  Container,
-  Header,
-  ListHeader,
   Card,
-  InputSearchContainer,
+  Container,
+  EmptyListContainer,
   ErrorContainer,
+  Header,
+  InputSearchContainer,
+  ListHeader,
+  SearchNotFoundContainer,
 } from './styles';
 import ContactsService from '../../services/ContactsService';
 
@@ -33,7 +37,7 @@ export default function Home() {
     [contacts, searchTerm],
   );
 
-  async function loadContacts() {
+  const loadContacts = useCallback(async () => {
     try {
       setIsLoading(true);
       const contactsList = await ContactsService.listContacts(orderBy);
@@ -44,11 +48,11 @@ export default function Home() {
     } finally {
       setIsLoading(false);
     }
-  }
+  }, [orderBy]);
 
   useEffect(() => {
     loadContacts();
-  }, [orderBy]);
+  }, [loadContacts]);
 
   function handleToggleOrderBy() {
     setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
@@ -65,16 +69,27 @@ export default function Home() {
   return (
     <Container>
       <Loader isLoading={isLoading} />
-      <InputSearchContainer>
-        <input
-          value={searchTerm}
-          type="text"
-          placeholder="Pesquisar contato..."
-          onChange={handleChangeSearchTerm}
-        />
-      </InputSearchContainer>
-      <Header $hasError={hasError}>
-        {!hasError && (
+      {contacts.length > 0 && !hasError && (
+        <InputSearchContainer>
+          <input
+            value={searchTerm}
+            type="text"
+            placeholder="Pesquisar contato..."
+            onChange={handleChangeSearchTerm}
+          />
+        </InputSearchContainer>
+      )}
+      <Header
+        $justifyContent={
+          // eslint-disable-next-line no-nested-ternary
+          hasError
+            ? 'flex-end'
+            : contacts.length > 0
+              ? 'space-between'
+              : 'center'
+        }
+      >
+        {!hasError && contacts.length > 0 && (
           <strong>
             {filteredContacts.length}{' '}
             {filteredContacts.length === 1 ? 'contato' : 'contatos'}
@@ -95,6 +110,25 @@ export default function Home() {
       )}
       {!hasError && (
         <>
+          {contacts.length < 1 && !isLoading && (
+            <EmptyListContainer>
+              <img src={emptyBox} alt="Empty box" />
+              <p>
+                Você ainda não tem nenhum contato cadastrado! Clique no botão{' '}
+                <strong>&ldquo;Novo contato&rdquo;</strong> acima para cadastrar
+                o seu primeiro.
+              </p>
+            </EmptyListContainer>
+          )}
+          {contacts.length > 0 && filteredContacts.length < 1 && (
+            <SearchNotFoundContainer>
+              <img src={magnifierQuestion} alt="Magnifier question" />
+              <span>
+                Nenhum resultado foi encontrado para{' '}
+                <strong>&ldquo;{searchTerm}&rdquo;</strong>.
+              </span>
+            </SearchNotFoundContainer>
+          )}
           {filteredContacts.length > 0 && (
             <ListHeader $orderBy={orderBy}>
               <button
