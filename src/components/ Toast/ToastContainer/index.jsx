0926df879df60a6642973ca4dad2930c97b39931
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-02-19 18:37:30 -0300

    feat: create event emitter for toast

--- src/components/ Toast/ToastContainer/index.jsx
+++ src/components/ Toast/ToastContainer/index.jsx
@@ -1,12 +1,42 @@
+import { useState, useEffect } from 'react';
+
 import ToastMessage from '../ToastMessage';
+
 import { Container } from './styles';
 
 export default function ToastContainer() {
+  const [messages, setMessages] = useState([
+    { id: Math.random(), type: 'default', text: 'Default text' },
+    { id: Math.random(), type: 'danger', text: 'Danger text' },
+    { id: Math.random(), type: 'success', text: 'Success text' },
+  ]);
+
+  useEffect(() => {
+    function handleAddToast(event) {
+      const { type, text } = event.detail;
+
+      setMessages((prevState) => [
+        ...prevState,
+        { id: Math.random(), type, text },
+      ]);
+    }
+
+    document.addEventListener('addtoast', handleAddToast);
+
+    return () => {
+      document.removeEventListener('addtoast', handleAddToast);
+    };
+  }, []);
+
   return (
     <Container>
-      <ToastMessage text="Default toast" />
-      <ToastMessage text="Error toast" type="danger" />
-      <ToastMessage text="Success toast" type="success" />
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          type={message.type}
+          text={message.text}
+        />
+      ))}
     </Container>
   );
 }
--- src/pages/NewContact/index.jsx
+++ src/pages/NewContact/index.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
 import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function NewContact() {
   async function handleSubmit(formData) {
@@ -14,14 +15,17 @@ export default function NewContact() {
         category_id: formData.categoryId,
       };
 
-      const response = await ContactsService.createContact(contact);
+      await ContactsService.createContact(contact);
 
-      // FIXME: Apagar log
-      console.log(response);
-    } catch (error) {
-      // TODO: Implementar lógica de erro
-      // alert('Ocorreu um erro ao cadastrar o contato');
-      alert(error);
+      toast({
+        type: 'success',
+        text: 'Contato cadastrado com sucesso!',
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao cadastrar o contato!',
+      });
     }
   }
 
