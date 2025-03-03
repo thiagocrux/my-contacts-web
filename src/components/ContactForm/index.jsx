31790879b8079fb7959828277d08b7d243ffc368
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-03-02 22:16:48 -0300

    feat: add temporary solution to erase fields on submit

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -77,6 +77,10 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
     });
 
     setIsSubmitting(false);
+    setName('');
+    setEmail('');
+    setPhone('');
+    setCategoryId('');
   }
 
   return (
