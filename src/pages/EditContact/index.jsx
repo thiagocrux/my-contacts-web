f9f322e4598b53664b655c4695c05ce279b74bfd
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-03-03 18:56:11 -0300

    feat: implement dynamic page header to edit form

--- src/pages/EditContact/index.jsx
+++ src/pages/EditContact/index.jsx
@@ -9,6 +9,7 @@ import toast from '../../utils/toast';
 
 export default function EditContact() {
   const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
   const contactFormRef = useRef(null);
 
   const { id } = useParams();
@@ -19,6 +20,7 @@ export default function EditContact() {
       try {
         const contact = await ContactsService.getContactById(id);
         contactFormRef.current.fillFormFields(contact);
+        setContactName(contact.name);
         setIsLoading(false);
       } catch {
         navigate('/');
@@ -40,7 +42,9 @@ export default function EditContact() {
   return (
     <>
       <Loader isLoading={isLoading} />
-      <PageHeader title="Editar Mateus Silva" />
+      <PageHeader
+        title={isLoading ? 'Carregando...' : `Editar ${contactName}`}
+      />
       <ContactForm
         ref={contactFormRef}
         buttonLabel="Salvar alterações"
